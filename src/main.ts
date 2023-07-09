a2c37af2d7b9c592f9b66d91439d52974e4f7b18
Jubeen Amatya <45592860+jubain@users.noreply.github.com>
2023-07-09 15:20:43 +0100

    updating port in main.ts

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  await app.listen(3001);
+  // await app.listen(3001);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
